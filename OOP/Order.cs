2ff40b38d83500236dc838c02d7b356bb9c92eee
Mầu Tiến Nam <130735681+NamMau@users.noreply.github.com>
2024-11-15 11:36:12 +0700

    add field

--- OOP/Order.cs
+++ OOP/Order.cs
@@ -9,14 +9,18 @@ namespace OOP
 {
     public class Order : Transaction
     {
+        internal string delivery_status;
+
         public int CustomerID { get; set; }
         public string OrderStatus { get; set; }
         public string PaymentStatus { get; set; }
-        public int PaymentMethodID { get; set; }
+        public string PaymentMethod { get; set; }
         public DateTime OverDueDate { get; set; }
         public DateTime? PaidAt { get; set; }
         public string DeliveryStatus { get; set; }
         public int ShippingProviderID { get; set; }
+        public int Paid_at { get; internal set; }
+        public int PaymentMethodID { get; internal set; }
     }
 
 }
